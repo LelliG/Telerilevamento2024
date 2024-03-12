0cea023f15a212797f13b7a0bad8490e0569075b
LelliG <106979112+LelliG@users.noreply.github.com>
2024-03-12 16:29:49 +0100

    Update 02_installing_packages.R

--- 02_installing_packages.R
+++ 02_installing_packages.R
@@ -1,3 +1,11 @@
 #installing new packages in R
 install.packages("terra") #argomento è il nome del pack, va nel Cran
 library(terra)
+
+# install.packages("devtools")
+library(devtools)
+
+# install the imageRy package from GitHub
+devtools::install_github("ducciorocchini/imageRy")
+
+library(imageRy)
