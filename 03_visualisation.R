a6e667bd6d49db2cbc9f7a08c1241efdddbfe068
LelliG <106979112+LelliG@users.noreply.github.com>
2024-03-14 16:32:10 +0100

    Update 03_visualisation.R

--- 03_visualisation.R
+++ 03_visualisation.R
@@ -4,5 +4,5 @@ library(imageRy)
 #List of data avialable in imageRy, ogni funzione inizia con im.
 im.list()
 
-#importing data mato 2006
+#importing data mato 2006 https://earthobservatory.nasa.gov
 mato <- im.import("matogrosso_ast_2006209_lrg.jpg")
