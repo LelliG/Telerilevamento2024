12034bbb37642c44abfd8f3fc03119288b3f0745
LelliG <106979112+LelliG@users.noreply.github.com>
2024-04-09 17:17:03 +0200

    Update 05_classification.R

--- 05_classification.R
+++ 05_classification.R
@@ -21,3 +21,17 @@ m1992c <- im.classify(m1992, num_cluster=2)
 m2006c <- im.classify(m2006, num_cluster=2)
 #class 1 = human
 #class 2 = forest
+
+
+
+#Calculating frequencies 1996
+f1992 <- freq(m1992c) #forest>>>>>>>>human
+tot1992 <- ncell(m1992c)
+prop1992 = f1992/ tot1992
+perc1992 = prop1992*100#forest=83% human=17%
+
+#Calculating frequencies 2006
+f2006 <- freq(m2006c) #forest>>>>>>>>human
+tot2006 <- ncell(m2006c)
+prop2006 = f2006/ tot2006
+perc2006 = prop2006*100#forest=55% human=45%
