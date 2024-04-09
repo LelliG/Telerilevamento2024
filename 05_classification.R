e3d3fcb0adde984bb43e3873cd0e79aa0f31ace5
LelliG <106979112+LelliG@users.noreply.github.com>
2024-04-09 16:43:19 +0200

    Update 05_classification.R

--- 05_classification.R
+++ 05_classification.R
@@ -1,1 +1,14 @@
+library('terra')
+library('imageRy')
+library('ggplot2')
 
+#Quantify land cover variability
+
+#import Sun and Mato
+im.list()
+m1992<- im.import("matogrosso_l5_1992219_lrg.jpg")
+sun<- im.import("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg")
+
+#classify
+im.classify(m1992, num_cluster=3)
+sunc <- im.classify(sun, num_cluster=3)#numeri delle classi variano in base all'algoritmo.(1,2,3)
