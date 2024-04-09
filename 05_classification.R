7aaf7cbe7d3e06dc01cd752ad7e2f59307391d92
LelliG <106979112+LelliG@users.noreply.github.com>
2024-04-09 16:52:23 +0200

    Update 05_classification.R

--- 05_classification.R
+++ 05_classification.R
@@ -10,5 +10,14 @@ m1992<- im.import("matogrosso_l5_1992219_lrg.jpg")
 sun<- im.import("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg")
 
 #classify
-im.classify(m1992, num_cluster=3)
 sunc <- im.classify(sun, num_cluster=3)#numeri delle classi variano in base all'algoritmo.(1,2,3)
+
+#classification on Mato in different eras
+m2006 <-im.import("matogrosso_ast_2006209_lrg.jpg")
+
+m1992c <- im.classify(m1992, num_cluster=2)
+#class 1 = human
+#class 2 = forest 
+m2006c <- im.classify(m2006, num_cluster=2)
+#class 1 = human
+#class 2 = forest
